a3ab851aaec7775675fd6ee98da6138a41064cb8
Felleria <tellytabbie@gmail.com>
2024-06-29 18:20:38 +0300

    adds python routing and views

--- server/app.py
+++ server/app.py
@@ -4,41 +4,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
 @app.route('/')
 def index():
-    return '<h1>Python Operations with Flask Routing and Views</h1>'
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
 
 @app.route('/print/<string:parameter>')
-def print_string(parameter):
-    print(parameter)
-    return f'<p>{parameter}</p>'
+def print_parameter(parameter):
+    print(f"{parameter}")
+    return parameter
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    numbers = ''.join(f'{num}<br>' for num in range(parameter))
-    return f'<p>{numbers}</p>'
+    numbers = [i for i in range(int(parameter))]
+    return "\n".join(map(str, numbers)) + '\n' 
 
-@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1, operation, num2):
-    if operation == '+':
-        result = num1 + num2
-    elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return 'Invalid operation', 400
-
-    return f'<p>{num1} {operation} {num2} = {result}</p>'
+@app.route('/math/<int:parameter1>/<string:operation>/<int:parameter2>')
+def math(parameter1, operation, parameter2):
+    if operation == "+":
+        return str(parameter1 + parameter2)
+    elif operation == "-":
+        return str(parameter1 - parameter2)
+    elif operation == "*":
+        return str(parameter1 * parameter2)
+    elif operation == "div":
+        return str(parameter1 / parameter2)
+    elif operation == "%":
+        return str(parameter1 % parameter2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(port=5555, debug=True)
